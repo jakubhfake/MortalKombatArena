42671cd1a0a8306554c693c873226a7c2ab85bfd
Jakub Haliniak <jakubh.fake@gmail.com>
2023-06-09 00:14:22 +0200

    add validation errors and constructor arguments

--- records/warrior.record.ts
+++ records/warrior.record.ts
@@ -1,3 +1,5 @@
+import {ValidationError} from "../utils/errors";
+
 export class WarriorRecord {
     public id?: string;
     /**
@@ -16,8 +18,20 @@ export class WarriorRecord {
         const sum = [force, defence, stamina, agility].reduce((prev, curr) => prev + curr, 0);
 
         if (sum !== 10) {
-
+            throw new ValidationError(`Suma wszystkich statystyk musi wynosić 10. Aktualnie wynosi ona ${sum}`);
         }
+
+        if(name.length <= 3 && name.length >= 50) {
+            throw new ValidationError(`Imię wojownika musi posiadać od 3 do 50 znaków. Aktualnie jest to ${name.length}`);
+        }
+        this.id = id;
+        this.name = name;
+        this.force = force;
+        this.defence = defence;
+        this.stamina = stamina;
+        this.agility = agility;
+        this.wins = wins;
+
     }
 
 }
