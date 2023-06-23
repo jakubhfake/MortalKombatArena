95507df452b6aeb5a891e8b9e9bc205882962bfd
Jakub Haliniak <jakubh.fake@gmail.com>
2023-06-23 05:05:11 +0200

    working on warrior fight

--- utils/fight.ts
+++ utils/fight.ts
@@ -4,17 +4,24 @@ export const fight = (warrior1: WarriorRecord, warrior2: WarriorRecord): string[
     const log: string[] = [];
 
     const warrior1TmpStats = {
-        hp:warrior1.stamina * 10,
+        hp: warrior1.stamina * 10,
         dp: warrior1.defence,
     };
 
     const warrior2TmpStats = {
-        hp:warrior2.stamina * 10,
+        hp: warrior2.stamina * 10,
         dp: warrior2.defence,
     };
 
-    let attaker = warrior1;
+    let attacker = warrior1;
     let defender = warrior2;
+
+    do {
+        const attackForce = attacker.force;
+
+        if (defender.)
+
+    } while (warrior1TmpStats.hp > 0 && warrior2TmpStats.hp > 0);
 
     return log;
 }
