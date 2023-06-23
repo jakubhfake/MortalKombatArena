2c128a57f0534cb163a663de1b9b0ec8c3fe146a
Jakub Haliniak <jakubh.fake@gmail.com>
2023-06-23 05:54:03 +0200

    working on warrior fight

--- utils/fight.ts
+++ utils/fight.ts
@@ -3,25 +3,34 @@ import {WarriorRecord} from "../records/warrior.record";
 export const fight = (warrior1: WarriorRecord, warrior2: WarriorRecord): string[] => {
     const log: string[] = [];
 
-    const warrior1TmpStats = {
+    const warrior1Obj = {
         hp: warrior1.stamina * 10,
         dp: warrior1.defence,
+        warrior: warrior1,
     };
 
-    const warrior2TmpStats = {
+    const warrior2Obj = {
         hp: warrior2.stamina * 10,
         dp: warrior2.defence,
+        warrior: warrior2,
     };
 
-    let attacker = warrior1;
-    let defender = warrior2;
+    let attacker = warrior1Obj;
+    let defender = warrior2Obj;
 
     do {
-        const attackForce = attacker.force;
+        const attackForce = attacker.warrior.force;
 
-        if (defender.)
+        if (defender.dp + defender.warrior.agility > attackForce) {
+            defender.dp -= attackForce;
+            if (defender.dp < 0) {
+                defender.hp += defender.dp
+            }
+        }
 
-    } while (warrior1TmpStats.hp > 0 && warrior2TmpStats.hp > 0);
+        [defender, attacker] = [attacker, defender];
+
+    } while (defender.hp > 0);
 
     return log;
 }
